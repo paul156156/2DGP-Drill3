829a3bd4184f6b37d87208a1776df98bf29edbbf
Blues <44077651+paul156156@users.noreply.github.com>
2024-09-24 11:50:02 +0900

    무한루프+함수_확인

--- character_grass.py
+++ character_grass.py
@@ -2,6 +2,19 @@ from pico2d import *
 
 open_canvas()
 
-# fill here
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
+def run_circle():
+    print('circle')
+    pass
+
+def run_rectangle():
+    print('rectangle')
+    pass
+
+while True:
+    run_rectangle()
+    run_circle()
 
 close_canvas()
