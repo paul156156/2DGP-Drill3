e8c5104b4cbd15aa6ca946e76c3e3541b0dca3b5
Blues <44077651+paul156156@users.noreply.github.com>
2024-09-24 12:12:14 +0900

    원운동_구현_완료

--- character_grass.py
+++ character_grass.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 open_canvas()
 
@@ -8,9 +9,16 @@ boy = load_image('character.png')
 def run_circle():
     print('CIRCLE')
 
-    clear_canvas_now()
-    boy.draw_now(400,300)
-    delay(0.1)
+    r,cx,cy = 300,800//2,600//2
+    
+    for degree in range(0, 360, 3):
+        theta = math.radians(degree)
+        x = r * math.cos(theta) + cx
+        y = r * math.sin(theta) + cy
+
+        clear_canvas_now()
+        boy.draw_now(x,y)
+        delay(0.1)
 
 def run_rectangle():
     print('RECTANGLE')
